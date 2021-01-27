adf035c5de5b8fca8ad7cf3a63293c23b76aeab3
mrarmin <52494368+mrarmin@users.noreply.github.com>
2021-01-27 14:33:42 +0100

    Dio 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -47,6 +47,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         
         }
 
@@ -62,6 +69,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
