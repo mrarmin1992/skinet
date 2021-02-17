8a02eb82e76396edeec41d1f6c6ca467d44b119b
mrarmin <52494368+mrarmin@users.noreply.github.com>
2021-02-17 12:10:58 +0100

    login sekcija

--- Core/Entities/Identity/Address.cs
+++ Core/Entities/Identity/Address.cs
@@ -1,7 +1,10 @@
+
+
 namespace Core.Entities.Identity
 {
     public class Address
     {
+       
         public int Id { get; set;}
         public string FirstName  { get; set; }
         public string LastName { get; set; }
